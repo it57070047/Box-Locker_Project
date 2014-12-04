c8163f4c59cf6c686811c9fcdf0dfedf47ce8922
it57070047 <s7070047@kmitl.ac.th>
2014-12-05 04:02:26 +0700

    Add Function Check In & Expire
    Can be check day of check in and expire .
    **Bug : When click skip day . Day change output(show) Not in Data!

--- Box Locker - Code.py
+++ Box Locker - Code.py
@@ -3,6 +3,7 @@ from Tkinter import *
 from time import *
 import sys
 
+checkEmpty = ['---EMPTY---'] * 61
 class Frame(object):
     def __init__(self):               
         root = Tk()
@@ -22,7 +23,7 @@ class Frame(object):
         self.month = timelocal[1]
         self.year = timelocal[0]
         Label(root, text="SKIP DAY").place(x = 475, y = 55)
-        Spinbox(root, from_=self.day, to=32, textvariable = skipDay,  command = lambda: self.showDay(skipDay.get())).place(x = 535, y = 55, width = 35)
+        Spinbox(root, from_=self.day, to=32, textvariable = skipDay,  command = lambda: self.showDay(skipDay.get(), root)).place(x = 535, y = 55, width = 35)
         month_year = 'December 2014'
         Label(root, text=strftime('Today :  %A %d ') + ('%s' % month_year)).place(x = 100, y = 55)
 
@@ -53,35 +54,56 @@ class Frame(object):
     def clickBox(self, number):
         self.colour.set('White' if self.colour.get() != 'White' else 'Grey')
         self.button_value[number-1].configure(bg='Grey')
-        print number
-        Box(number)
+        dayInWeek = strftime('%A ')
+        Day = strftime('%d ')
+        Month = strftime('%B ')
+        Year = strftime('%Y')
+        Today = dayInWeek + Day + Month + Year
+        print number, Today
+        Box(number, Today)
 
-    def showDay(self, v):
+    def showDay(self, day, root):
         week = ['Wednessday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday', 'Tueday']
         month = ['December', 'January']
         year = ['2014', '2015']
         Label(root, text = ('%50s' % (' '*120))).place(x = 100, y = 55)
-        Label(root, text = ('Today : %s %s %s %s' % (week[(v - self.day)% 7], [v%32, 1][v%32==0], month[[0, 1][v>31]], year[[0, 1][v>31]]))).place(x = 100, y = 55)
+        Label(root, text = ('Today : %s %s %s %s' % (week[(day - self.day)% 7], [day%32, 1][day%32==0], month[[0, 1][day>31]], year[[0, 1][day>31]]))).place(x = 100, y = 55)
     
 class Box(object):
-    def __init__(self, value):
+    def __init__(self, value, date):
         box = Tk()
         box.geometry("400x230")
         box.resizable(width=FALSE, height=FALSE)
         box.title("Box Locker : " + str(value))
         Label(box, text = "Box : " + str(value)).place(x = 200, y = 35, anchor = CENTER)
-        Label(box, text = "Name ", ).place(x = 115, y = 55, anchor = CENTER)
+
+        Label(box, text = "Name ", ).place(x = 125, y = 55, anchor = CENTER)
         self.name = StringVar(box)
-        Entry(box, textvariable = self.name).place(x = 200, y = 55, anchor = CENTER)
-        Submit = Button(box, text = "Save", command = lambda: self.getData(self.name.get())).place(x = 150, y = 175, anchor = CENTER)
-        Button(box, text = "Close", command = lambda: self.closeButton(box)).place(x = 200 ,y = 175, anchor = CENTER)
+        Entry(box, textvariable = self.name).place(x = 210, y = 55, anchor = CENTER)
+
+        Label(box, text = checkEmpty[value]).place(x = 200, y = 105, anchor = CENTER)
+
+        if checkEmpty[value] == '---EMPTY---':
+            Button(box, text = "Save", command = lambda: self.getData(self.name.get(), value, box, date)).place(x = 200 ,y = 175, anchor = CENTER)
+        else:
+            Button(box, text = "Close", command = lambda: self.closeButton(box)).place(x = 200 ,y = 175, anchor = CENTER)
         box.mainloop()
 
     def closeButton(self, box):
         box.destroy()
 
-    def getData(self, name):
-        print name
+    def getData(self, name, number, box, today):
+        week = ['Wednessday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday', 'Tueday']
+        month = ['December', 'January']
+        year = ['2014', '2015']
+        checkEmpty[number] = ''
+        expire_inweek, expire_day, expire_month, expire_year = today.split()
+        #print week[(week.index(expire_inweek) + 5) % 7]
+        expireDay = ''
+        expireDay += week[(week.index(expire_inweek) + 5) % 7] +' '+ str(int(expire_day)+5) +' '+ str(expire_month) +' '+str(expire_year)
+        
+        checkEmpty[number] += 'Name : ' + name + '\n\nCheck In : ' + today + '\n\nExpire : ' + expireDay
+        box.destroy()
 
 #--------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
